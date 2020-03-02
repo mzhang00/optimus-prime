d01dee80c22f17a144405ad065137229a5e7b126
Michael Zhang <mzhang00@stuy.edu>
2020-03-02 01:45:56 -0500

    readd base draw

--- draw.py
+++ draw.py
@@ -6,23 +6,23 @@ def draw_lines( matrix, screen, color ):
     if len(matrix) < 2:
         print('Need at least 2 points to draw')
         return
-    
+
     point = 0
     while point < len(matrix) - 1:
         draw_line( matrix[point][0],
                    matrix[point][1],
                    matrix[point+1][0],
                    matrix[point+1][1],
-                   screen, color)    
+                   screen, color)
         point+= 2
-        
+
 def add_edge( matrix, x0, y0, z0, x1, y1, z1 ):
     add_point(matrix, x0, y0, z0)
     add_point(matrix, x1, y1, z1)
-    
+
 def add_point( matrix, x, y, z=0 ):
     matrix.append( [x, y, z, 1] )
-    
+
 
 
 
@@ -46,7 +46,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     if ( abs(x1-x0) >= abs(y1 - y0) ):
 
         #octant 1
-        if A > 0:            
+        if A > 0:
             d = A + B/2
 
             while x < x1:
