ba7fa9d8beb95341e09ab23fa049caa8a986f546
Michael Zhang <mzhang00@stuy.edu>
2020-03-02 02:10:29 -0500

    test

--- matrix.py
+++ matrix.py
@@ -1,101 +1,85 @@
+from display import *
+from matrix import *
+from draw import *
+
 """
-A matrix will be an N sized list of 4 element lists.
-Each individual list will represent an [x, y, z, 1] point.
-For multiplication purposes, consider the lists like so:
-x0  x1      xn
-y0  y1      yn
-z0  z1  ... zn
-1  1        1
+Goes through the file named filename and performs all of the actions listed in that file.
+The file follows the following format:
+     Every command is a single character that takes up a line
+     Any command that requires arguments must have those arguments in the second line.
+     The commands are as follows:
+         line: add a line to the edge matrix -
+               takes 6 arguemnts (x0, y0, z0, x1, y1, z1)
+         ident: set the transform matrix to the identity matrix -
+         scale: create a scale matrix,
+                then multiply the transform matrix by the scale matrix -
+                takes 3 arguments (sx, sy, sz)
+         translate: create a translation matrix,
+                    then multiply the transform matrix by the translation matrix -
+                    takes 3 arguments (tx, ty, tz)
+         rotate: create a rotation matrix,
+                 then multiply the transform matrix by the rotation matrix -
+                 takes 2 arguments (axis, theta) axis should be x y or z
+         apply: apply the current transformation matrix to the edge matrix
+         display: clear the screen, then
+                  draw the lines of the edge matrix to the screen
+                  display the screen
+         save: clear the screen, then
+               draw the lines of the edge matrix to the screen
+               save the screen to a file -
+               takes 1 argument (file name)
+         quit: end parsing
+See the file script for an example of the file format
 """
-import math
-
-def make_scale( x, y, z ):
-    m = new_matrix()
-    m[0][0] = x
-    m[1][1] = y
-    m[2][2] = z
-    m[3][3] = 1
-    return m
-
-def make_translate( x, y, z ):
-    m = new_matrix()
-    ident(m)
-    m[3][0] = x
-    m[3][1] = y
-    m[3][2] = z
-    return m
-
-def make_rotX( theta ):
-    theta = math.radians(theta)
-    m = new_matrix()
-    ident(m)
-    m[1][1] = math.cos(theta)
-    m[2][1] = math.sin(theta) * -1
-    m[1][2] = math.sin(theta)
-    m[2][2] = math.cos(theta)
-    return m
-
-def make_rotY( theta ):
-    theta = math.radians(theta)
-    m = new_matrix()
-    ident(m)
-    m[0][0] = math.cos(theta)
-    m[0][2] = math.sin(theta) * -1
-    m[2][0] = math.sin(theta)
-    m[2][2] = math.cos(theta)
-    return m
-
-def make_rotZ( theta ):
-    theta = math.radians(theta)
-    m = new_matrix()
-    ident(m)
-    m[0][0] = math.cos(theta)
-    m[1][0] = math.sin(theta) * -1
-    m[1][1] = math.sin(theta)
-    m[0][1] = math.cos(theta)
-    return m
-
-#print the matrix such that it looks like
-#the template in the top comment
-def print_matrix( matrix ):
-    s = ''
-    for r in range( len( matrix[0] ) ):
-        for c in range( len(matrix) ):
-            s+= str(matrix[c][r]) + ' '
-        s+= '\n'
-    print(s)
-
-#turn the paramter matrix into an identity matrix
-#you may assume matrix is square
-def ident( matrix ):
-    for r in range( len( matrix[0] ) ):
-        for c in range( len(matrix) ):
-            if r == c:
-                matrix[c][r] = 1
+def parse_file( fname, points, transform, screen, color ):
+    f=open(fname,"r")
+    count=0
+    ident(transform)
+    for instruct in f:
+        count+=1
+        if instruct == "line\n":
+            instruct=f.readline().strip("\n").split(" ")
+            edge=[int(instruct[0]),int(instruct[1]),int(instruct[2]),1]
+            points.append(edge)
+            edge=[int(instruct[3]), int(instruct[4]),int(instruct[5]),1]
+            points.append(edge)
+        elif instruct == "ident\n":
+            ident(transform)
+        elif instruct == "scale\n":
+            instruct=f.readline().strip("\n").split(" ")
+            scale=make_scale(int(instruct[0]),int(instruct[1]),int(instruct[2]))
+            matrix_mult(scale,transform)
+        elif instruct == "move\n":
+            instruct=f.readline().strip("\n").split(" ")
+            translate=make_translate(int(instruct[0]),int(instruct[1]),int(instruct[2]))
+            matrix_mult(translate,transform)
+        elif instruct == "rotate\n":
+            instruct=f.readline().strip("\n").split(" ")
+            if(instruct[0]=="x"):
+                rotate=make_rotX(int(instruct[1]))
+                matrix_mult(rotate,transform)
+            elif(instruct[0]=="y"):
+                rotate=make_rotY(int(instruct[1]))
+                matrix_mult(rotate,transform)
+            elif(instruct[0]=="z"):
+                rotate=make_rotZ(int(instruct[1]))
+                matrix_mult(rotate,transform)
             else:
-                matrix[c][r] = 0
-
-#multiply m1 by m2, modifying m2 to be the product
-#m1 * m2 -> m2
-def matrix_mult( m1, m2 ):
-
-    point = 0
-    for row in m2:
-        #get a copy of the next point
-        tmp = row[:]
-
-        for r in range(4):
-            m2[point][r] = (m1[0][r] * tmp[0] +
-                            m1[1][r] * tmp[1] +
-                            m1[2][r] * tmp[2] +
-                            m1[3][r] * tmp[3])
-        point+= 1
-
-
-def new_matrix(rows = 4, cols = 4):
-    m = []
-    for c in range( cols ):
-        m.append( [] )
-        for r in range( rows ):
-            m[c].append( 0 )
-    return m
+                print("Invalid Argument at line "+str(count))
+        elif instruct == "apply\n":
+            matrix_mult(transform,points)
+        elif instruct == "display\n":
+            clear_screen(screen)
+            draw_lines(points,screen,color)
+            display(screen)
+        elif instruct == "save\n":
+            instruct=f.readline().strip("\n")
+            clear_screen(screen)
+            draw_lines(points,screen,color)
+            save_extension(screen,instruct)
+        elif instruct == "quit\n":
+            break
+        else:
+            print(instruct)
+            print("Invalid Command at line "+str(count))
+    f.close()
